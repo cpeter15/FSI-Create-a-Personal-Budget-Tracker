43031bd4146566f66064b47385c6d9f02d0afeb1
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-03-31 20:40:29 -0400

    added loops to index.js

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+answer = 0
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+answer = 0
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
